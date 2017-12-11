edaf85464b8592338a2f73eb188ed3c3019ff67f
a-mellor <alex.mellor@honeycomb.tv>
2017-12-11 13:54:21 +0000

    Hamming feedback

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,8 +1,8 @@
 class Hamming
   def self.compute(a, b)
     raise ArgumentError if  a.length != b.length
- 
-    [a, b].min.size.times.count { |i| a[i] != b[i] }
+    
+    a.size.times.count { |i| a[i] != b[i] }
   end
 end
 
