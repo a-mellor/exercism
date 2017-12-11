b74a75aa99249c9460ab0b1b8b6bb9a5a4006de1
a-mellor <alex.mellor@honeycomb.tv>
2017-12-11 21:53:50 +0000

    Remove case statement

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,30 +1,22 @@
 class Complement
-  def self.of_dna(strand)
-    rna_complement = ''
-    strand.each_char { |nucleotide| rna_complement << dna_to_rna_replacement(nucleotide) }
+  COMPLIMENTS = {
+    'G' => 'C',
+    'C' => 'G',
+    'T' => 'A',
+    'A' => 'U'
+  }.freeze
 
-    if rna_complement.length == strand.length
-      rna_complement
-    else
-      ''
-    end
-  end
-  
-  def self.dna_to_rna_replacement(nucleotide)
-    case nucleotide
-    when 'C'
-      'G'
-    when 'G'
-      'C'
-    when 'T'
-      'A'
-    when 'A'
-      'U'
-    else ''
-    end
+  def self.of_dna(strand)
+    strand.chars.map do |nucleotide| 
+      COMPLIMENTS.fetch(nucleotide)
+    end.join
+    
+  rescue KeyError
+    ''
   end
 end
 
 module BookKeeping
   VERSION = 4
 end
+
